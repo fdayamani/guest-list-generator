060abfddbf283e37eae102d0518829fb5ce381a9
Fatema Damani <fatema.damani@gmail.com>
2019-08-30 18:21:06 +0100

    Add a react front end to the project with a simple form to input a guest's name (doesn't yet link to server)

---
+++ guest-list-generator/src/main/javascript/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import GuestAdder from './GuestAdder'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <GuestAdder/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ guest-list-generator/src/main/javascript/GuestAdder.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+class GuestAdder extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {guestName: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleAddGuest = this.handleAddGuest.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({guestName: event.target.value});
+    }
+
+    handleAddGuest(event) {
+        console.log('A guest was added: ' + this.state.guestName) ;
+    }
+
+    render() {
+        return (
+            <form onSubmit = {this.handleAddGuest}>
+                <label>
+                    <input type="text" name="name" value={this.state.value} onChange = {this.handleChange}/>
+                </label>
+                    <input type="submit" value="Add Guest" />
+            </form>
+        );
+    }
+}
+
+export default GuestAdder;
