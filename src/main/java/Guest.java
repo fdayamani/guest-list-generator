c9d45d77c432d73a1c5cb13316e1b8d3bd77cb9b
Fatema Damani <fatema.damani@gmail.com>
2019-09-05 12:16:26 +0100

    Fixes test

--- src/main/java/Guest.java
+++ src/main/java/Guest.java
@@ -4,4 +4,20 @@ public class Guest {
     public Guest(String name) {
         this.name = name;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Guest guest = (Guest) o;
+
+        return name != null ? name.equals(guest.name) : guest.name == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return name != null ? name.hashCode() : 0;
+    }
 }
--- src/test/java/GuestListResourceTest.java
+++ src/test/java/GuestListResourceTest.java
@@ -5,17 +5,17 @@ import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertTrue;
 
 public class GuestListResourceTest {
+    List<Guest> guestList = new ArrayList();
+    GuestListResource generator = new GuestListResource(guestList);
 
     @Test public void
     adds_guest_to_guest_list() {
-        List<Guest> guestList = new ArrayList();
-        GuestListResource generator = new GuestListResource(guestList);
-        Guest patrick = new Guest("Patrick");
 
-        generator.add(patrick);
+        generator.add("Patrick");
 
-        assertThat(guestList.contains(patrick), is(true));
+        assertThat(guestList.contains(new Guest("Patrick")), is(true));
     }
 }
