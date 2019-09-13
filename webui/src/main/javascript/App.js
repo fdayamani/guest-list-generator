865a5d1a1f82745a6a292d5e8519d05e0787a1f7
Fatema Damani <fatema.damani@gmail.com>
2019-09-13 16:10:39 +0100

    Add node express backend with a test api to make sure everything is correctly wired together
    Followed tutorial here
    https://www.freecodecamp.org/news/create-a-react-frontend-a-node-express-backend-and-connect-them-together-c5798926047c/

--- webui/src/main/javascript/App.js
+++ webui/src/main/javascript/App.js
@@ -2,14 +2,33 @@ import React from 'react';
 import './App.css';
 import GuestAdder from './GuestAdder'
 
-function App() {
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { apiResponse: "" };
+    }
+
+    callAPI() {
+        fetch("http://localhost:9000/testAPI")
+            .then(res => res.text())
+            .then(res => this.setState({ apiResponse: res }));
+    }
+
+    componentWillMount() {
+        this.callAPI();
+    }
+
+  render() {
   return (
-    <div className="App">
+  <div className="App">
       <header className="App-header">
         <GuestAdder/>
+        <p className="App-intro">{this.state.apiResponse}</p>
       </header>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
